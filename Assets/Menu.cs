c1994e4dcbc220b73aa849c5484fb0af34fbe4cb
EstebanVC <estebanvasquezc20010106@gmail.com>
2023-02-04 22:18:59 -0500

    avances

---
+++ Assets/Menu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class Menu : MonoBehaviour
+{   
+   
+  public void Cambio () 
+    {
+        Debug.Log("Cambio");
+        SceneManager.LoadScene("Menu");
+
+    }
+
+    
+    
+}
