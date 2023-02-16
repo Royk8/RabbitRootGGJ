79067f7fd5ef8fbf06166997be0ec41ce05be276
EstebanVC <estebanvasquezc20010106@gmail.com>
2023-02-16 17:38:50 -0500

    Tocó comunicacion serial :c

--- Assets/_Game/Scripts/Movimiento Arduino/Movimiento Arduino.ino
+++ Assets/_Game/Scripts/Movimiento Arduino/Movimiento Arduino.ino
@@ -1,6 +1,9 @@
 int sensorIndice = 0;
 int sensorCorazon = 1;
 int sensorAnular = 2;
+//const int sensorIndice = 4;
+//int sensorCorazon = 33;
+//const int sensorAnular = 14;
 int lecturaIndice;
 int lecturaCorazon;
 int lecturaAnular;
@@ -16,8 +19,9 @@ void setup()
   pinMode(sensorIndice, INPUT);
   pinMode(sensorCorazon, INPUT);
   pinMode(sensorAnular, INPUT);
-  analogReference(DEFAULT);
+  //analogReference();
   Serial.begin(115200);
+  //analogReadResolution(11);
 }
 
 void loop() 
@@ -25,13 +29,10 @@ void loop()
   lecturaIndice = analogRead(sensorIndice);
   lecturaCorazon = analogRead(sensorCorazon);
   lecturaAnular = analogRead(sensorAnular);
-  
-  flexionIndice = map(lecturaIndice, 968, 1015, 0, 180);
-  flexionCorazon = map(lecturaCorazon,  970, 1016, 0, 180);
-  flexionAnular = map(lecturaAnular,  950, 1023, 0, 180); // 236 y 634 son numeros para hallar segun la conversion analoga
-  //flexion_indice = flexion_indice + 7.0; // Numero para linealizar la conversion
-  //flexion_corazon = flexion_corazon + 7.0; // Numero para linealizar la conversion
-  //flexion_anular = flexion_anular + 7.0; // Numero para linealizar la conversio
+
+  flexionIndice = map(lecturaIndice, 968, 1010, 0, 180);
+  flexionCorazon = map(lecturaCorazon,  970, 1012, 0, 180);
+  flexionAnular = map(lecturaAnular, 968, 1013, 0, 180); // 236 y 634 son numeros para hallar segun la conversion analoga
 
   if(flexionIndice > 180)
   {
@@ -60,19 +61,25 @@ void loop()
     flexionAnular = 0;
   }
 
-  int datoIndice = (int) round(flexionIndice*9/180);
-  int datoCorazon = (int) round(flexionCorazon*9/180);
-  int datoAnular = (int) round(flexionAnular*9/180);
+  //int datoIndice = (int) round(flexionIndice*9/180);
+  //int datoCorazon = (int) round(flexionCorazon*9/180);
+  //int datoAnular = (int) round(flexionAnular*9/180);
 
-  enteroEnviado = (datoIndice*100)+(datoCorazon*10)+(datoAnular);
-
+  //enteroEnviado = (datoIndice*100)+(datoCorazon*10)+(datoAnular);
   enviado += flexionIndice;
   enviado += separador; 
   enviado += flexionCorazon;
   enviado += separador;
-  enviado += datoAnular;
-
-  Serial.println(enteroEnviado);
-  //enviado =  "";
-  delay(500);
+  enviado += flexionAnular;
+/*
+  enviado += lecturaIndice;
+  enviado += separador; 
+  enviado += lecturaCorazon;
+  enviado += separador;
+  enviado += lecturaAnular;
+  */
+  //Serial.println(enteroEnviado);
+  Serial.println(enviado);
+  enviado =  "";
+  delay(300);
 }
